f3a5a31e138777cf6c0bbd46e5321c202a1e1b8d
Juubes <sekopaa247@gmail.com>
2019-04-13 01:24:08 +0300

    created total stats

---
+++ Nexus/src/com/juubes/nexus/data/AbstractTotalStats.java
@@ -0,0 +1,64 @@
+package com.juubes.nexus.data;
+
+import java.util.HashMap;
+import java.util.UUID;
+
+public class AbstractTotalStats {
+//	kills, deaths, monuments, wins, losses, winTime, lossTime, biggestKillStreak;
+
+	protected final HashMap<Integer, AbstractStats> allStats;
+	protected final UUID uuid;
+
+	public AbstractTotalStats(UUID uuid, HashMap<Integer, AbstractStats> allStats) {
+		this.uuid = uuid;
+		this.allStats = allStats;
+	}
+
+	public int getKills() {
+		int sum = 0;
+		for (AbstractStats stats : allStats.values()) {
+			sum += stats.kills;
+		}
+		return sum;
+	}
+
+	public int getBiggestKillStreak() {
+		int biggest = 0;
+		for (AbstractStats stats : allStats.values()) {
+			biggest = Math.max(biggest, stats.biggestKillStreak);
+		}
+		return biggest;
+	}
+
+	public int getDeaths() {
+		int sum = 0;
+		for (AbstractStats stats : allStats.values()) {
+			sum += stats.deaths;
+		}
+		return sum;
+	}
+
+	public int getWins() {
+		int sum = 0;
+		for (AbstractStats stats : allStats.values()) {
+			sum += stats.wins;
+		}
+		return sum;
+	}
+
+	public int getLosses() {
+		int sum = 0;
+		for (AbstractStats stats : allStats.values()) {
+			sum += stats.losses;
+		}
+		return sum;
+	}
+	
+	public UUID getUUID() {
+		return uuid;
+	}
+	
+	public HashMap<Integer, AbstractStats> getAllStats() {
+		return allStats;
+	}
+}
