41eadf8bbabb16dd52d9c2ef0dae27af6f01ff3d
juubes <juubes@juubes-MS-7B48>
2019-07-04 23:41:44 +0300

    Added a new constructor

--- src/com/juubes/nexus/InitOptions.java
+++ src/com/juubes/nexus/InitOptions.java
@@ -1,5 +1,7 @@
 package com.juubes.nexus;
 
+import java.util.List;
+
 import com.juubes.nexus.data.AbstractDatabaseManager;
 import com.juubes.nexus.logic.GameLoader;
 
@@ -12,6 +14,11 @@ public class InitOptions {
 	private final String[] mapIDs;
 	private final String defaultPrefix;
 	private final GameLoader gameLoader;
+
+	public InitOptions(GameLoader gameLoader, AbstractDatabaseManager dbManager, List<String> mapIDs,
+			String defaultPrefix) {
+		this(gameLoader, dbManager, mapIDs.toArray(new String[mapIDs.size()]), defaultPrefix);
+	}
 
 	public InitOptions(GameLoader gameLoader, AbstractDatabaseManager dbManager, String[] mapIDs,
 			String defaultPrefix) {
